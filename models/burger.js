420a42a663815c87e51946847c07a61ffe47ae5a
elenabaurkot <elenabaurkot@gmail.com>
2020-03-01 20:05:40 -0500

    burger model updated

--- models/burger.js
+++ models/burger.js
@@ -1,22 +1,30 @@
-// Also inside burger.js, create the code that will call the ORM functions using burger specific input for the ORM.
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
 
-// Export at the end of the burger.js file.
-
-// Controller setup
-var orm = require("../config/orm.js")
-
-function showAll() {
-    orm.selectAll("burgers")
+var burger = {
+  selectAll: function(cb) {
+    orm.selectAll("burgers", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  insertOne: function(cols, vals, cb) {
+    orm.insertOne("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  updateOne: function(objColVals, condition, cb) {
+    orm.updateOne("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
+// ,
+//   delete: function(condition, cb) {
+//     orm.delete("burgers", condition, function(res) {
+//       cb(res);
+//     });
+//   }
 };
 
-// will need to update this
-function addBurger() {
-    orm.insertOne("burgers", "burger_name", whateverAnswerIs)
-}
-
-// will need to update this
-function updateOne(){
-    orm.updateOne(column, columnName, id)
-}
-
+// Export 
 module.exports = burger;
